446080d770063a874a881553d1a3a7b9ac25c6f8
Nolascao <69157914+Nolascao@users.noreply.github.com>
2022-08-24 00:04:01 -0300

    finalização do esqueleto

--- Implementacao/TipoDisciplina.java
+++ Implementacao/TipoDisciplina.java
@@ -1,4 +1,11 @@
 
 public enum TipoDisciplina {
+    OBRIGATORIA(4),
+    OPCIONAL(2);
+
+    int quantidadeDeMateriasPossiveis;
+    TipoDisciplina(int quantidadeDeMateriasPossiveis){
+        this.quantidadeDeMateriasPossiveis = quantidadeDeMateriasPossiveis;
+    }
 
 }
