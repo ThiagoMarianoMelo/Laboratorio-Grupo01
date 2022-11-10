d14385a285c9f0ec344bb4203248ff3452b60125
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-11-09 22:13:53 -0300

    Finalizando método de obter usuário por meio do login

--- Implementacao/04/frontend/src/repositories/UserRepository.ts
+++ Implementacao/04/frontend/src/repositories/UserRepository.ts
@@ -1,9 +1,14 @@
+import { HttpStatusCode } from "axios";
+import { axios } from "../config/axios";
 import { UserModel } from "../models/UserModel";
 import { ILoginUserRequest } from "../pages/Login";
 
 class UserRepository {
     public async Login(request: ILoginUserRequest): Promise<UserModel | null> {
-        
+        const url = '/login';
+
+        const response = await axios.post(url, request);
+        return response.data;
     }
 }
 
