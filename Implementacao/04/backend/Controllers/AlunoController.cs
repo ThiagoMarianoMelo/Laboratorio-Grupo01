a7db37027e1b29dac3fcd136d1e75a5cf590be57
ThiagoMelo <thiago.17mel@gmail.com>
2022-11-12 20:47:18 -0300

    Finalização desenvolvimento sprint 4 backend

--- Implementacao/04/backend/Controllers/AlunoController.cs
+++ Implementacao/04/backend/Controllers/AlunoController.cs
@@ -8,29 +8,30 @@ using Sprint4.Services.Usuario.DescontarSaldo;
 using Sprint4.Services.Vantagem.Interface.IEcontrarVantagem;
 using Sprint4.Services.Transacao.CadastrarTransacao;
 using Sprint4.Services.Vantagem.EncontrarVantagem;
-using Sprint4.Services.Vantagem.CadastrarVantagem;
+using Sprint4.Services.Usuario.Interfaces.IDescontarSaldo;
 using Sprint4.Services.Transacao.Interfaces.ICadastrarTransacao;
+using Sprint4.Services.Usuario.DescontarSaldo;
 
 [ApiController]
 public class AlunoController : ControllerBase 
 {
     public IEcontrarVantagem encontrarVantagemEscolhida;
     public ICadastrarTransacao cadastrarTransacao;
+    public IDescontarSaldo descontarSaldo;
 
     public AlunoController(){
         encontrarVantagemEscolhida = new EncontrarVantagem();
         cadastrarTransacao = new CadastrarTransacao();
+        descontarSaldo = new DescontarSaldo();
     }
 
 [HttpPost]
 [Route("comprarVantagem")]
     public void adquirirVantagem( [FromBody] ComprarVantagemModel modelAdquiriVantagemModel){
 
-        DescontarSaldo ds = new DescontarSaldo();
-
         ListarVantagemModel vantagemEscolhida = encontrarVantagemEscolhida.getVantagem(modelAdquiriVantagemModel.idVantagem);
 
-        ds.descontarSaldoConta(vantagemEscolhida.preco, modelAdquiriVantagemModel.idAluno);
+        descontarSaldo.descontarSaldoConta(vantagemEscolhida.preco, modelAdquiriVantagemModel.idAluno);
 
         TrasacaoModel transacaoGerada = new TrasacaoModel();
 
